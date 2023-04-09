06266cf38e332b226825e5c36deaefefb98d8e1e
teslapuns <130309952+teslapuns@users.noreply.github.com>
2023-04-09 21:40:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,11 +37,11 @@ function generateResult() {
       result = puns[selectedIndex] + " " + input1;
     }
     if (inc == incidents[selectedIndex].length - 1) {
-      result += pun_time + " " + selectedValue + "[Инцидент " + inc + " (предпоследний)]";
+      result += pun_time + " " + selectedValue + " [Инцидент " + inc + " (предпоследний)]";
     } else if (inc == incidents[selectedIndex].length ) {
-      result += pun_time + " " + selectedValue + "[Инцидент " + inc + " (последний)]";
+      result += pun_time + " " + selectedValue + " [Инцидент " + inc + " (последний)]";
     } else {
-      result += pun_time + " " + selectedValue + "[Инцидент " + inc + "]";
+      result += pun_time + " " + selectedValue + " [Инцидент " + inc + "]";
     }
   }
   if (checkbox1) {
